047cb28785f459607de93778152bf59db6336add
hpt <hpt@ bu.edu>
2024-03-20 23:47:53 -0400

    Spacing

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ x = np.arange(0,20,0.1)
 z = np.random.rand(200)*5
 y = np.sin(x) + z
 plt.plot(x, y, ".")
+
 #design matrix
 a = np.vstack([np.ones(len(x)), x]).T
 # A^TAx = A^Tb
 xhat = np.dot(np.dot(np.linalg.inv(np.dot(a.T, a)), a.T), y)
+
 plt.plot(x, xhat[1]*x + xhat[0], "r")
-
-
 
 acf = np.zeros_like(x)
 for i in range(len(x)):
@@ -27,8 +27,6 @@ for i in range(len(x)):
     numerator = np.sum((y_curr - mean_y_curr) * (y_lag - mean_y_lag))
     denominator = np.sqrt(np.sum((y_curr - mean_y_curr)**2) * np.sum((y_lag - mean_y_lag)**2))
     acf[i] = numerator / denominator
-
-
 
 plt.figure()
 plt.bar(x, acf, width=0.1, align='center')
